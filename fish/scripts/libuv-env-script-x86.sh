73f905cd047459d846843602fa7335d1762eeeeb
byHyakimaro <moderrevolution090217@gmail.com>
2023-10-24 06:14:39 -0300

    dd

--- fish/scripts/libuv-env-script-x86.sh
+++ fish/scripts/libuv-env-script-x86.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 sudo apt-get install -y autoconf automake libtool
 
-APP_DIR=../../app
+git clone https://github.com/libuv/libuv.git
+cd libuv
 
-git clone https://github.com/libuv/libuv
-cd libuv && git checkout v1.x
-
-sh autogen.sh
+./autogen.sh
 ./configure
 make
 make check
 make install
-
-./build/uv_run_tests
-
-cp -r include $APP_DIR/libuv/
-cp -r .libs/libuv.a $APP_DIR/libuv/
